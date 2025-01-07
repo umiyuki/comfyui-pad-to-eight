8694f90a99bd96ee857887656b5847b67b1f84ba
umiyuki <egoblock@infoseek.jp>
2025-01-07 18:13:31 +0900

    Update PadToEight node and project settings

--- __init__.py
+++ __init__.py
@@ -3,10 +3,7 @@ from .nodes import *
 
 #  Map all your custom nodes classes with the names that will be displayed in the UI.
 NODE_CLASS_MAPPINGS = {
-    "(Down)Load My Model": MyModelLoader,
-    "Calculate Plus": CalculatePlus,
-    "Calculate Minus": CalculateMinus,
-    "Example Output Node": ExampleOutputNode,
+    "Pad To Eight": PadToEight,
 }
 
 
